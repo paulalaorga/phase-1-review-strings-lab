7b6437b24f63bdbc29aa19730872a77a29c3ea50
paula <paulalaorga@gmail.com>
2023-08-12 21:11:59 +1000

    index

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = "User";
+const welcomeMessage = 'Welcome to Flatbook, ${currentUser}!';
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ${currentUSER}!';
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
